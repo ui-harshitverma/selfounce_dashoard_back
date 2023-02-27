de32dde7a4d0fc57f33208968322f856109d2fcf
ui-harshitverma <harshitverma@cedcommerce.com>
2023-02-27 16:08:16 +0530

    removed some files

--- routers/productGridRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {getPoductGridController} = require('../controllers/productGridController');
-const router = express.Router();
-
-// user all routes
-router.get("/productlist", getPoductGridController);
-
-module.exports = router;
